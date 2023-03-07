280b46b3398177359614a08d0b7f0117846c3d73
creative_otabek <creative.otabek@gmail.com>
2023-03-07 15:08:02 +0500

    service debug

--- src/main/java/com/job/delivery/DeliveryApplication.java
+++ src/main/java/com/job/delivery/DeliveryApplication.java
@@ -3,21 +3,22 @@ package com.job.delivery;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 @EnableJpaRepositories
+@ComponentScan(basePackages = {"com.job.delivery.service"})
 public class DeliveryApplication {
 
-	@Bean
-	public BCryptPasswordEncoder bCryptPasswordEncoder(){
-		return new BCryptPasswordEncoder();
-	}
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(DeliveryApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DeliveryApplication.class, args);
+    }
 
 }
